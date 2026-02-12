16e3603764767b3c4541b3536dbe2d7f2c923d24
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-12 15:28:54 +0100

    chore(spring): add template Spring boot project

---
+++ src/main/java/oslomet/data1700/DATA1700_demo_march/Data1700DemoMarchApplication.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.DATA1700_demo_march;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700DemoMarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700DemoMarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oslomet/data1700/DATA1700_demo_march/Data1700DemoMarchApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.DATA1700_demo_march;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700DemoMarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
